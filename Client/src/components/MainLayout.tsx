3bb01c9b22a338b922cb202bed99bc3cdabb882e
unknown <boatengstephen707@gmail.com>
2022-07-15 06:35:27 -0700

    created header and sidebar

--- Client/src/components/MainLayout.tsx
+++ Client/src/components/MainLayout.tsx
@@ -4,6 +4,7 @@ import {
   AuthMachineSchema,
   AuthMachineContext,
 } from "../Machines/AuthMachine";
+import Navigation from "./Navigation";
 import {
   Interpreter,
   ResolveTypegenMeta,
@@ -11,6 +12,22 @@ import {
   BaseActionObject,
   ServiceMap,
 } from "xstate";
+import { makeStyles } from "@material-ui/core";
+const mainStyles = makeStyles((theme) => ({
+  layout_container: {
+    display: "flex",
+    height: "100vh",
+  },
+  sidebar: {
+    minWidth: "18%",
+    background: "white",
+    height: "100%",
+  },
+  main: {
+    width: "100%",
+    height: "100%",
+  },
+}));
 
 interface Props {
   authService: Interpreter<
@@ -28,10 +45,16 @@ interface Props {
   children: React.ReactNode;
 }
 const MainLayout: React.FC<Props> = ({ authService, children }) => {
+  const classes = mainStyles();
   return (
-    <div>
-      <div>Mainlayout</div>
-      <div>{children}</div>
+    <div className={classes.layout_container}>
+      <div className={classes.sidebar}></div>
+      <div className={classes.main}>
+        <div className="nav">
+          <Navigation />
+        </div>
+        <div>{children}</div>
+      </div>
     </div>
   );
 };
